3b720e065cf894196e5c62fd08f2e2e32c9dac72
ShadowDoc <prudky.tomas@email.cz>
2026-08-25 17:09:27 +0200

    set endpoint on PROD

--- fantasy-table/src/app/components/Serie.jsx
+++ fantasy-table/src/app/components/Serie.jsx
@@ -12,7 +12,8 @@ const Serie = ({ onDataFetched, retryCount = 3, retryDelay = 2000 }) => {
 
     const fetchData = async (attempt = 1) => {
       try {
-        const response = await axios.get('http://localhost:5000/api/serie-a/');
+        const response = await axios.get('https://fantasy-table-server.vercel.app/api/serie-a/');
+        //const response = await axios.get('http://localhost:5000/api/serie-a/');
 
         if (!isCancelled && onDataFetchedRef.current) {
           onDataFetchedRef.current(response.data);
